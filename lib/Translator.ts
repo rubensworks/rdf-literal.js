f6a256a59873485365239d20f018c88808c0261e
Ruben Taelman <ruben.taelman@ugent.be>
2019-06-11 16:39:26 +0200

    Add basic translator

---
+++ lib/Translator.ts
@@ -0,0 +1,73 @@
+import * as RDF from "rdf-js";
+import {ITypeHandler} from "./ITypeHandler";
+
+/**
+ * Translates between an RDF literal and a JavaScript primitive.
+ */
+export class Translator implements ITypeHandler {
+
+  private readonly supportedRdfDatatypes: RDF.NamedNode[];
+  private readonly fromRdfHandlers: {[rdfDatatype: string]: ITypeHandler};
+  private readonly toRdfHandlers: {[javaScriptType: string]: ITypeHandler[]};
+
+  constructor() {
+    this.supportedRdfDatatypes = [];
+    this.fromRdfHandlers = {};
+    this.toRdfHandlers = {};
+  }
+
+  public static incorrectRdfDataType(literal: RDF.Literal) {
+    throw new Error(`Invalid RDF ${literal.datatype.value} value: '${literal.value}'`);
+  }
+
+  public registerHandler(handler: ITypeHandler, rdfDatatypes: RDF.NamedNode[], javaScriptDataTypes: string[]) {
+    for (const rdfDatatype of rdfDatatypes) {
+      this.supportedRdfDatatypes.push(rdfDatatype);
+      this.fromRdfHandlers[rdfDatatype.value] = handler;
+    }
+    for (const javaScriptDataType of javaScriptDataTypes) {
+      let existingToRdfHandlers = this.toRdfHandlers[javaScriptDataType];
+      if (!existingToRdfHandlers) {
+        this.toRdfHandlers[javaScriptDataType] = existingToRdfHandlers = [];
+      }
+      existingToRdfHandlers.push(handler);
+    }
+  }
+
+  public fromRdf(literal: RDF.Literal, validate?: boolean): any {
+    const handler = this.fromRdfHandlers[literal.datatype.value];
+    if (handler) {
+      return handler.fromRdf(literal, validate);
+    } else {
+      return literal.value;
+    }
+  }
+
+  public toRdf(value: any, dataFactory: RDF.DataFactory): RDF.Literal {
+    const handlers = this.toRdfHandlers[typeof value];
+    if (handlers) {
+      for (const handler of handlers) {
+        const ret = handler.toRdf(value, dataFactory);
+        if (ret) {
+          return ret;
+        }
+      }
+    }
+    throw new Error(`Invalid JavaScript value: '${value}'`);
+  }
+
+  /**
+   * @return {NamedNode[]} An array of all supported RDF datatypes.
+   */
+  public getSupportedRdfDatatypes(): RDF.NamedNode[] {
+    return this.supportedRdfDatatypes;
+  }
+
+  /**
+   * @return {string[]} An array of all supported JavaScript types.
+   */
+  public getSupportedJavaScriptPrimitives(): string[] {
+    return Object.keys(this.toRdfHandlers);
+  }
+
+}
